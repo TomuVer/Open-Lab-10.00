ba9c251b3d6f9056563c03c760586f19c14bd33d
TomuVer <113421396+TomuVer@users.noreply.github.com>
2023-01-19 08:28:07 +0100

    Update Book.cs

--- Open-Lab-10.00/Book.cs
+++ Open-Lab-10.00/Book.cs
@@ -1,4 +1,3 @@
-using Open_Lab_10._02;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,19 +6,47 @@ using System.Threading.Tasks;
 
 namespace Open_Lab_10._02
 {
-    internal class Program
+    internal class Book
     {
-        static void Main(string[] args)
-        {
-            Book LOTR = new Book();
-            
-            LOTR.Title("title: Lord Of The Rings");
-            LOTR.Pages(1172);
-            LOTR.Category("category: epic high-fantasy novel");
-            LOTR.Author("author: J. R. R. Tolkien");
-            LOTR.RelaseDate("relase date: 29. July 1954");
-            LOTR.Vypis();
+        private string title;
+        private string category;
+        private string author;
+        private string relaseDate;
+        private int pages;
 
+        public string Title(string xtitle)
+        {
+            this.title = xtitle;
+            return title;
+        }
+        public string Category(string xcate)
+        {
+            this.category = xcate;
+            return category;
+        }
+        public string Author(string xauthor)
+        {
+            this.author = xauthor;
+            return author;
+        }
+        public string RelaseDate(string xrelaseDate)
+        {
+            this.relaseDate = xrelaseDate;
+            return relaseDate;
+        }
+        public int Pages(int xpages)
+        {
+            this.pages = xpages;
+            return pages;
+        }
+
+        public void Vypis()
+        {
+            Console.WriteLine(Category(category));
+            Console.WriteLine(Author(author));
+            Console.WriteLine(Title(title));
+            Console.WriteLine(RelaseDate(relaseDate));
+            Console.WriteLine("number of pages: " + Pages(pages));
         }
     }
 }
