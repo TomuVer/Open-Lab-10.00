11b8bf021258f63a803d2ccdd47850a5f3b02691
TomuVer <sim.private7@gmail.com>
2023-01-19 09:05:11 +0100

    OOP-10.04

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -10,7 +10,7 @@ namespace Open_Lab_10._00
     {
         static void Main(string[] args)
         {
-            //Nový objekty z triedy Book
+            //Nové objekty z triedy Book
             Book LOTR2 = new Book();
             Book LOTR3 = new Book("Ondrejove Dobrodružstvá", 120);
             Book HOBBIT = new Book("Ondrejove Dobrodružstvá", "Ondrej Barčišiak", "BioGrafia", 120, 2021);
